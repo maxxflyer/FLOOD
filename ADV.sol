d2f21571a92148df7526ce5c76a56bf53588fdbd
maxxflyer <maxxflyer@gmail.com>
2016-11-26 05:14:52 +0100

    added censorship 2

--- ADV.sol
+++ ADV.sol
@@ -1,21 +1,24 @@
 contract ADV{
+address public controller;
 mapping(address => adv[])public advs;
-uint start;
+mapping(address => string[])public censored;
+mapping(address => uint)public start;
+mapping(address => address)public ownerships;
 
 struct ADV{
 uint end;
 string addr;
 }
 
-function Zika(){start=0;}
+function Zika(){}
 
 function addAdv(address a,string s) returns (bool){
 adv Tadv=advs[a][0];
 if(block.number>Tadv.end){
-uint i=start;
-for(uint x=start;x<i+1;x++){
+uint i=start[a];
+for(uint x=start[a];x<i+1;x++){
 Tadv=advs[a][x];
-if(block.number>Tadv.end){i++;start++;}
+if(block.number>Tadv.end){i++;start[a]++;}
 }
 }
 advs.push({0,0,s});
@@ -25,9 +28,9 @@ return true;
 function getAdv(address a)constant returns (string){
 adv Tadv=advs[a][start];
 string Ts;
-uint i=start;
+uint i=start[a];
 if(block.number>Tadv.end){
-for(uint x=start;x<i+1;x++){
+for(uint x=start[a];x<i+1;x++){
 Tadv=advs[a][x];
 if(block.number>Tadv.end)i++;
 }
@@ -36,22 +39,31 @@ if(block.number>Tadv.end)i++;
 return Tadv.addr;
 }
 
-function removeAdv(){
+function verifyOwnership(address a){
 //effettua chiamata esterna
-//individua owner
+//individua owner di a
 //se non è owner throw
-address a;
-if(msg.sender!=a)throw;
-start++;
+if(msg.sender!=o)throw;
+ownerships[a]=msg.sender;
 }
 
-function censorAndCover(address a,uint u,string default){
-//effettua chiamata esterna
-//individua owner
-//se non è owner throw
-address a;
-if(msg.sender!=a)throw;
+function removeAdv(address a){
+if(!ownerships[a])throw;
+if(msg.sender!=ownerships[a])throw;
+adv Tadv=advs[a][start[a]];
+if(block.number>Tadv.end)
+start[a]++;
+}
+
+function censorAndCover(address a,uint u,string default)returns (bool){
+if((!ownerships[a])&&(msg.sender!=controller))throw;
+adv Tadv=advs[a][u];
+censored[a].push(Tadv.addr);
 advs[address][u]=default;
+}
+
+function getCensored(address a,uint u)constant returns (string){
+return(censored[a][u]);
 }
 
 }
